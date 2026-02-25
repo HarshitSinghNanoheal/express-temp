9b1d83c2e2dd78fd43d2f22dba03744f8aeccda7
Harshit <harshit.kumar.singh@nanoheal.com>
2026-02-25 11:29:44 +0530

    Added additional endpoint

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,28 @@ const PORT = 3005;
 
 app.post("/", async (req: Request, res: Response) => {
   try {
-    
+    // Write your code here
   } catch (e) {
     console.log(e);
   }
 });
+
+// Curl command for this endpoint
+// curl --location 'http://localhost:3005' \
+// --header 'Content-Type: application/json' \
+// --data '{"query" : "Write a 100-word poem."}'
+
+app.post("/event", async (req: Request, res: Response) => {
+  try {
+    // Write your code here
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+// Curl command for this event stream endpoint
+// curl -N -X POST http://localhost:3005/event \
+//   -H "Content-Type: application/json" \
+//   -d '{"query":"Write a long poem about the forest"}'
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
